d5a4ac859c26c2a054094ff8812624e3d532e3a1
ZanesBestFriend <niccostacey@gmail.com>
2023-02-07 09:42:50 -0600

    Still Gorm

--- src/story/Story.java
+++ src/story/Story.java
@@ -5,14 +5,18 @@ import java.io.File;
 import java.util.ArrayList;
 import graphics.Graphics;
 import graphics.Colors;
+import story.Gorm;
+
+
 
 public class Story {
-    
+
     public static void main(String[] args) throws Exception {
             //Intro:
                 //Fancy Welcome
 
             Graphics.text(Colors.ANSI_BLUE +  "temp studio name" + Colors.ANSI_RESET + " presents temp game name");
+
 
             //Choose Character
     
