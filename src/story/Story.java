c794b316a94e3908df8743bda0204519c153885f
ZanesBestFriend <niccostacey@gmail.com>
2023-02-07 09:35:14 -0600

    Story and Colors

--- src/story/Story.java
+++ src/story/Story.java
@@ -1,5 +1,25 @@
 package story;
 
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+import graphics.Graphics;
+import graphics.Colors;
+
 public class Story {
     
+    public static void main(String[] args) throws Exception {
+            //Intro:
+                //Fancy Welcome
+
+            Graphics.text(Colors.ANSI_BLUE +  "temp studio name" + Colors.ANSI_RESET + " presents temp game name");
+
+            //Choose Character
+    
+            //Choose Weapon 
+    
+            //Setting 
+        }
+        
+        
 }
