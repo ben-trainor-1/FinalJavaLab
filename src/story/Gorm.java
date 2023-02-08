8cfecc6eb28ad7327092c760625449a54b75f0b8
ZanesBestFriend <niccostacey@gmail.com>
2023-02-08 09:40:45 -0600

    New Gorm!

--- src/story/Gorm.java
+++ src/story/Gorm.java
@@ -2,5 +2,7 @@ package story;
 
 public class Gorm {
 
-
+    public static String name = "Gorm";
+    public static String height = "6' 7'";
+    public static String appearance = "Bald with red braid";
 }
--- src/story/Story.java
+++ src/story/Story.java
@@ -9,18 +9,59 @@ import story.Gorm;
 
 
 
+
 public class Story {
 
     public static void main(String[] args) throws Exception {
             //Intro:
-                //Fancy Welcome
-
-            Graphics.text(Colors.ANSI_BLUE +  "temp studio name" + Colors.ANSI_RESET + " presents temp game name");
-
+        
+            
+            //Fancy Welcome
+            Graphics.text(Colors.ANSI_BLUE +  "temp studio name" + Colors.ANSI_RESET + " presents temp game name" + "\n" + "please choose a character..." 
+                            + "\n" + Colors.ANSI_YELLOW + "Enter 1 to choose Gorm (male) or 2 to choose Gro (Female)" + Colors.ANSI_RESET);
 
             //Choose Character
+            
+            //Call scanner
+            //Use integers
+            Scanner in = new Scanner(System.in);
+            int input;
+            input = in.nextInt();
+
+            if(input == 1){
+                //Choosing Gorm
+                Graphics.text("You have chosen " + Gorm.name);
+            }
+            else if(input == 2){
+                //Choosing Gro 
+                Graphics.text("You have chosen ");
+            }
+            else{
+                //input error
+                Graphics.text("Please choose Gorm or Gro");
+            }
     
             //Choose Weapon 
+
+            //prompt user
+            Graphics.text(Colors.ANSI_YELLOW + "Enter 1 for a greatspear or 2 for a battleaxe" + Colors.ANSI_RESET);
+
+            //Reset input
+            input = 0;
+            input = in.nextInt();
+
+            if(input == 1){
+                //Choosing Greatspear
+                Graphics.text("You  have chosen spear");
+            }
+            else if(input == 2){
+                //Choosing Battleaxe
+                Graphics.text("You have chosen axe");
+            }
+            else{
+                //input error
+                Graphics.text("Please choose spear or axe");
+            }
     
             //Setting 
         }
