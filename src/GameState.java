6ca2b6b536485d7cff57ca493531279faa50f9a5
Ben Trainor <benjammintrain@gmail.com>
2023-02-10 09:24:11 -0600

    GameState Universal tracking

---
+++ src/GameState.java
@@ -0,0 +1,10 @@
+public class GameState {
+
+    public static String name = "";
+    public static String pNoun = "";
+    public static String objpNoun = "";
+    public static String weapon = "";
+    public static int health = 0;
+    public static int attack = 0;
+    
+}
