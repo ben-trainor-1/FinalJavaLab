e9d622496c07b9e3e3d5127cdce2f485e0facab3
ZanesBestFriend <niccostacey@gmail.com>
2023-02-10 09:10:11 -0600

    Beginning Story!

--- src/story/Gorm.java
+++ src/story/Gorm.java
@@ -3,6 +3,8 @@ package story;
 public class Gorm {
 
     public static String name = "Gorm";
+    public static String pNoun = "he";
+    public static String objectPNoun = "him";
     public static String height = "6' 7'";
     public static String appearance = "Bald with red braid";
 }
--- src/story/Story.java
+++ src/story/Story.java
@@ -14,58 +14,82 @@ import story.Gro;
 public class Story {
 
     public static void main(String[] args) throws Exception {
+
+        ////NOTE: Storing character data is yet to be implemented!
+        //String [] info = {" "," "," "," "," "," "};
             //Intro:
         
             
-            //Fancy Welcome
-            Graphics.text(Colors.ANSI_BLUE +  "temp studio name" + Colors.ANSI_RESET + " presents temp game name" + "\n" + "please choose a character..." 
-                            + "\n" + Colors.ANSI_YELLOW + "Enter 1 to choose Gorm (male) or 2 to choose Gro (Female)" + Colors.ANSI_RESET);
+        //Fancy Welcome
+        Graphics.text(Colors.ANSI_BLUE +  "temp studio name" + Colors.ANSI_RESET + " presents temp game name" + "\n" + "please choose a character..." 
+                        + "\n" + Colors.ANSI_YELLOW + "Enter 1 to choose Gorm (male) or 2 to choose Gro (Female)" + Colors.ANSI_RESET);
 
-            //Choose Character
+        //Choose Character
+
+        //remember character
+        //ArrayList<String> gormOrGroArray = new ArrayList<>();
+        
+        //Call scanner
+        //Use integers
+        Scanner in = new Scanner(System.in);
+        int input;
+        input = in.nextInt();
+
+        if(input == 1){
+
+            //NOTE: Storing character data is yet to be implemented!
+            // info[0] = Gorm.name;
+            // info[1] = Gorm.pNoun;
+            // info[2] = Gorm.objectPNoun;
+
+            //Choosing Gorm
+            Graphics.text("You have chosen Gorm! Gorm is a muscular bald man with one long red braid");
             
-            //Call scanner
-            //Use integers
-            Scanner in = new Scanner(System.in);
-            int input;
-            input = in.nextInt();
 
-            if(input == 1){
-                //Choosing Gorm
-                Graphics.text("You have chosen " + Gorm.name);
-            }
-            else if(input == 2){
-                //Choosing Gro 
-                Graphics.text("You have chosen " + Gro.name);
-            }
-            else{
-                //input error
-                Graphics.text("Please choose Gorm or Gro");
-            }
-    
-            //Choose Weapon 
-
-            //prompt user
-            Graphics.text(Colors.ANSI_YELLOW + "Enter 1 for a greatspear or 2 for a battleaxe" + Colors.ANSI_RESET);
-
-            //Reset input
-            input = 0;
-            input = in.nextInt();
-
-            if(input == 1){
-                //Choosing Greatspear
-                Graphics.text("You  have chosen spear");
-            }
-            else if(input == 2){
-                //Choosing Battleaxe
-                Graphics.text("You have chosen axe");
-            }
-            else{
-                //input error
-                Graphics.text("Please choose spear or axe");
-            }
-    
-            //Setting 
         }
-        
-        
+        else if(input == 2){
+
+            //NOTE: Storing character data is yet to be implemented!
+            // info[0] = Gro.name;
+            // info[1] = Gro.pNoun;
+            // info[2] = Gro.objectPNoun;
+            
+            //Choosing Gro 
+            Graphics.text("You have chosen Gro! Gro is a graceful woman with Blonde braids and one bald spot");
+            //gormOrGroArray.add("Gro");
+        }
+        else{
+            //input error
+            Graphics.text("Please choose Gorm or Gro");
+        }
+
+
+        //Choose Weapon 
+
+        //prompt user
+        Graphics.text(Colors.ANSI_YELLOW + "Enter 1 for a greatspear or 2 for a battleaxe" + Colors.ANSI_RESET);
+
+        //Reset input
+        input = 0;
+        input = in.nextInt();
+
+        if(input == 1){
+            //Choosing Greatspear
+            Graphics.text("You  have chosen spear");
+        }
+        else if(input == 2){
+            //Choosing Battleaxe
+            Graphics.text("You have chosen axe");
+        }
+        else{
+            //input error
+            Graphics.text("Please choose spear or axe");
+        }
+
+        //Setting 
+        Graphics.text(Colors.ANSI_PURPLE + "Your story starts in the misty mountains of Iceland in a town called Asbjorn \n" + 
+                        "It is a small town sparsely populated. While the inhabitants are kind an welcoming, You are a warrior who wishes to fight." + Colors.ANSI_RESET);
+    }
+    
+    
 }
