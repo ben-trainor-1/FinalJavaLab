451eeffa8b56cb210248504dae1900b78188e33e
potatoLemon23 <coralreader@lords-house.net>
2023-02-14 09:45:39 -0600

    Updated Stats Line

--- src/graphics/GraphicsTest.java
+++ src/graphics/GraphicsTest.java
@@ -1,6 +1,6 @@
 package graphics;
 public class GraphicsTest {
     public static void main(String[] args) throws Exception {
-        Graphics.displayCharacter("Gorm", 100);
+        Graphics.displayCharacter("Viggo", 100);
     }
 }
