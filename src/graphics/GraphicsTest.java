1e7883bfa83623839e8c5fc0b0d79a76a71504a7
Ben Trainor <benjammintrain@gmail.com>
2023-02-14 09:07:15 -0600

    Testing backgrounds

--- src/graphics/GraphicsTest.java
+++ src/graphics/GraphicsTest.java
@@ -1,7 +1,13 @@
 package graphics;
+import graphics.Graphics;
 
 public class GraphicsTest {
+
     public static void main(String[] args) throws Exception {
+
         Graphics.displayCharacter("Gorm", 100);
+        Graphics.displayBackground("mountains");
+
     }
+    
 }
