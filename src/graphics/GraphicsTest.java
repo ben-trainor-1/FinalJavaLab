8d08f79ff1d4de9bf11da0ba5d9b02afc611c671
icyDingo20 <kimory.d.stanley2006@gmail.com>
2023-02-15 09:14:12 -0600

    Test

--- src/graphics/GraphicsTest.java
+++ src/graphics/GraphicsTest.java
@@ -2,11 +2,6 @@ package graphics;
 public class GraphicsTest {
     public static void main(String[] args) throws Exception {
         Graphics.displayCharacter("Gorm", 100);
-<<<<<<< HEAD
-        Graphics.displayDeath("Tomb");
-        Graphics.displayWeapons("Bomb");
-=======
         Graphics.displayCharacter("Gorm", 100);
->>>>>>> 6332d3a71d9683ed3624f7c1afc092005a8355c0
     }
 }
