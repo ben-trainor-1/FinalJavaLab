fde2ddf34e237d0d9f384321e71fd20f392ef7d9
potatoLemon23 <coralreader@lords-house.net>
2023-02-15 09:05:28 -0600

    Updated test graphics

--- src/graphics/GraphicsTest.java
+++ src/graphics/GraphicsTest.java
@@ -2,5 +2,7 @@ package graphics;
 public class GraphicsTest {
     public static void main(String[] args) throws Exception {
         Graphics.displayCharacter("Gorm", 100);
+        Graphics.displayDeath("Tomb");
+        Graphics.displayWeapons("Bomb");
     }
 }
