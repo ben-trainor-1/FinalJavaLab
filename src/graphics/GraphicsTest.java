3b065a2306683564a14e470aa295defc3aa0cedd
icyDingo20 <kimory.d.stanley2006@gmail.com>
2023-03-17 09:29:25 -0500

    Edited Graphics.Test

--- src/graphics/GraphicsTest.java
+++ src/graphics/GraphicsTest.java
@@ -3,5 +3,6 @@ public class GraphicsTest {
     public static void main(String[] args) throws Exception {
         Graphics.displayCharacter("Gorm", 100);
         Graphics.displayWin("Yay");
+       
     }
 }
