58306d229c61d48d7a4a6cd3b95a21a93d8ae647
Ben Trainor <benjammintrain@gmail.com>
2023-03-20 09:25:09 -0500

    Fade out pops but works. Fade in sounds great. Usage in doc comment.

--- src/audio/FadeTest.java
+++ src/audio/FadeTest.java
@@ -6,11 +6,13 @@ public class FadeTest {
     public static void main(String[] args) throws Exception {
 
         Player play = new Player();
-        Clip clip = play.playAudio("./src/audio/loop_soft_piano.wav", 0);
+        Clip clip = play.playAudio("./src/audio/loop_soft_piano.wav", 0, -80.0f);
 
-        System.out.println("Lowering volume...");
-        Player.fadeOutAudio(clip, 4000);
-        System.out.println("Volume lowered.");
+        // System.out.println("Lowering volume...");
+        // Player.fadeOutAudio(clip, 4000);
+        // System.out.println("Volume lowered.");
+        
+        Player.fadeInAudio(clip, 4000);
         
         do {
             // Wait for clip to end
--- src/audio/Player.java
+++ src/audio/Player.java
@@ -6,18 +6,31 @@ import java.io.*;
 import javax.sound.sampled.FloatControl;
 
 /**
- * Usage
- * 0. Import the Clip class to your file that wll use audio
- * 1. Create a new Player object in your file
- * 2. Call playAudio and give it two arguments
- *    a. The file path for the audio file
- *    b. How many times it should loop (-1 for continuous)
- * 3. playAudio will return a Clip object
- *    Use this object to keep track of playback
+ * <b>Usage</b>
+ * <ol>
+ *  <li>Import the Clip class to your file that wll use audio</li>
+ *  <li>Create a new Player object in your file</li>
+ *  <li>Call playAudio and give it two arguments
+ *      <p>1) The file path for the audio file</p>
+ *      <p>2) How many times it should loop (-1 for continuous)</p>
+ *  <li>playAudio will return a Clip object</li>
+ * </ol> 
+ * Use this object to keep track of playback.
  */
 public class Player {
 
-    Clip playAudio(String audioFilePath, int loopCount) throws Exception {
+    /**
+     * Use this for initializing an audio file. Requires a path, loop count, and start volume. 
+     * Use <code>-80.0f</code> when wanting to fade in a file so that it starts silent and fades in.
+     * Call this method with that level and then call <code>fadeInAudio()</code> with fade length.
+     * Otherwise, use <code>0.0f</code> for the start volume or whatever sounds right.
+     * @param audioFilePath
+     * @param loopCount
+     * @param startVolume
+     * @return
+     * @throws Exception
+     */
+    Clip playAudio(String audioFilePath, int loopCount, float startVolume) throws Exception {
 
         File audioFile = new File(audioFilePath);
 
@@ -26,6 +39,8 @@ public class Player {
             AudioInputStream audioInput = AudioSystem.getAudioInputStream(audioFile);
             Clip clip = AudioSystem.getClip();
             clip.open(audioInput);
+            FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volume.setValue(startVolume);
             clip.loop(loopCount);
             return clip;
         }
@@ -38,6 +53,12 @@ public class Player {
 
     }
 
+    /**
+     * Handles fading audio clips out. Use the original clip that is playing the audio file and the duration of the fade in milliseconds.
+     * @param clip
+     * @param fadeDuration
+     * @throws InterruptedException
+     */
     public static void fadeOutAudio(Clip clip, float fadeDuration) throws InterruptedException {
 
         FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
@@ -68,6 +89,46 @@ public class Player {
         } while (x < fadeDuration); // volume.getValue() > -80.0F
 
         clip.stop();
+        
+    }
+
+    /**
+     * Handles fading audio clips in. Use the original clip that is playing the audio file and the duration of the fade in milliseconds.
+     * @param clip
+     * @param fadeDuration
+     * @throws InterruptedException
+     */
+    public static void fadeInAudio(Clip clip, float fadeDuration) throws InterruptedException {
+
+        FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        
+        float floor = -80.0f; // Lowest valid decibel level
+        float fadeGap = 1.0f; // Gap between fade moments
+        long sleepDuration = (long) fadeGap;
+        float q = floor;
+        float s = fadeDuration / 1000.0f; // Convert fade duration to seconds
+        float x = 0;
+        float newVolume;
+
+        volume.setValue(-80.0f);
+        clip.start();
+
+        // Set volume according to the function
+        // y = sqrt{q^{2} * s} * 1/s * sqrt{(-x/1000) + s} + q
+        
+        do {
+            
+            // Calculate new volume level
+            newVolume = (float) (Math.sqrt(q * q * s) * (1/s) * Math.sqrt((x/1000)) + q);
+            // Set new volume level
+            volume.setValue(newVolume);
+
+            // Note time elapsed
+            x += fadeGap;
+            Thread.sleep(sleepDuration);
+
+            
+        } while (volume.getValue() < 0.0f); // volume.getValue() > -80.0F
         
     }
     
