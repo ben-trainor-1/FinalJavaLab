fb31c68786f7b8f72790a0864612521fa4eb6095
katycat16 <katejoyprunty@gmail.com>
2023-03-30 09:05:04 -0500

    hela's palace

--- src/story/storypt2.java
+++ src/story/storypt2.java
@@ -36,7 +36,7 @@ public class storypt2 {
             input = in.nextInt();
 
                 if(input == 1){ 
-                    Graphics.text(Colors.ANSI_YELLOW + "You have chosen to to take the path on the right." 
+                    Graphics.text(Colors.ANSI_YELLOW + "You have chosen to take the path on the right." 
                     + " Suddenly, an angry dwarf comes out of the shadows\n"
                     + "He jumps you immediately and while you think he doesn't have a lot of strenghth, he continues to suprise you.\n" 
                     + "After a rather long battle, you defeat the little, yet mighty dwarf and walk towards the room on your left\n"
@@ -94,7 +94,7 @@ public class storypt2 {
         //Outside the gates: Nothing of note, but a trickling stream of dirty water
                 
             Graphics.textInline(Colors.ANSI_PURPLE + " After your fun little banter with Loki, you walk to the entrance of the gates.\n"
-                                + "There is nothing but a dirty stream but you're excited to find out what lies beyonf the gates.\n"
+                                + "There is nothing but a dirty stream but you're excited to find out what lies beyond the gates.\n"
                                 + "You push them open and discover the bottom of Hel was so much more than what you expected.\n" );
      //Eastern Hel: Docks
              //Naglfar, the ship made of fingernails and toenails resides here.
@@ -137,8 +137,50 @@ public class storypt2 {
            }
                                  
         } while(true);
-    }
+   
+   // Approach palace after maze 
+        Graphics.text(Colors.ANSI_PURPLE + "When you finally get through and defeat the maze, you reach Hela's palace.\n"
+                          + "There are guards at the door but you have to get in to get permission to fight aboard Naglfar.\n"
+                          + "You appoarch the guards and try to make up a sob story but they don't buy it.\n"
+                          + "You finally give up and tell them the truth but they still don't care. Your only option is to fight your way in.\n"
+                          + "After a long fight between the two of them, you are a little injuired but you manage to get past the doors.\n"
+                          + "\"Who are you and how did you get in here?\"\n"
+                          + "\"My name is [...]. I was told I had to come here to get permission from you, great Hela.\"\n"
+                          + "You approach her at her throne and kneel on one knee.\n"
+                          + "\"You are correct. But don't think you'll get my consent so easily. I have a riddle for you.\"\n"
+                          + "\"What does man love more than life, hate more than death or mortal strife; that which contented men desire; the poor have, the rich require; the miser spends, the spendthrift saves, and all men carry to their graves?\"\n"
+                          + "You have three options for the answer.\n");
     
+                
+                
+        do {
+            Scanner in = new Scanner (System.in);
+            int input;
+            input = in.nextInt();
+            Graphics.text(Colors.ANSI_YELLOW + "There are three options. Only one is the correct answer.\n"
+                                             + "option 1: Nothing.\n"
+                                             + "option 2: Money.\n"
+                                             + "A moral life.\n");
+
+            if (input == 1) {
+                Graphics.textInline(Colors.ANSI_PURPLE + "Hela slowly starts to nod her head.\n"
+                                                       + "\"You are very smart. You have my permission to aboard the Naglfar.\"\n"
+                                                       + "She hands you a letter and let you go on your way.\n");
+                Graphics.displayWin("yay");
+                break;
+              }
+
+            else if (input == 2 || input == 3) {
+                Graphics.textInline("Hela shakes her head and says that you will not be able to baord Naglfar and fight");
+                Graphics.displayDeath("Dead");
+                Graphics.textInline(Colors.ANSI_BLUE + "\"Because you got the riddle wrong, you must the meanest elf.\"\n");
+            
+            
+            }
+
+          
+        } while(true);
+   }  
 }
     
 
