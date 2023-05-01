a8e2dc1623a0873d152c0b887b663cb4df904b90
icyDingo20 <kimory.d.stanley2006@gmail.com>
2023-05-01 09:45:18 -0500

    Gamestate Name added to combat test

--- src/combat/Test.java
+++ src/combat/Test.java
@@ -1,11 +1,14 @@
 package combat;
 
 import graphics.Graphics;
+import state.GameState;
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
 
+        GameState.name = "Gorm";
+        
         Graphics.textSpeed = 10;
         FullCombat.enemyGameState("Reginald", 999, 100, 1, 0, 5, 100, 0);
         FullCombat.fight();
