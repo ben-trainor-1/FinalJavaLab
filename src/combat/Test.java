3f86efaae7331ea6a28fb9ffd9788efb2ffbde3c
icyDingo20 <kimory.d.stanley2006@gmail.com>
2023-05-04 09:22:10 -0500

    Fixed Test.java

--- src/combat/Test.java
+++ src/combat/Test.java
@@ -10,7 +10,7 @@ public class Test {
         GameState.name = "Gorm";
         
         Graphics.textSpeed = 10;
-        FullCombat.enemyGameState("Reginald", 999, 100, 1, 0, 5, 100, 0);
+        FullCombat.enemyGameState("Reginald", 30, 150, 1, 50, 0, 5, 0, 1, 0);
         FullCombat.fight();
 
     }
