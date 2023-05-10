32bad50c1d6805e8b8763ba751c78c82b6bdcff9
Ben Trainor <benjammintrain@gmail.com>
2023-05-10 18:40:35 -0500

    Updated victory theme to match 8bit feel

--- src/combat/Test.java
+++ src/combat/Test.java
@@ -11,7 +11,7 @@ public class Test {
         
         Graphics.textSpeed = 10;
 
-        FullCombat.enemyGameState("Dwarf",180,100,4,20,2,6,25,4,2,50);
+        FullCombat.enemyGameState("Dwarf",10,0,4,20,2,6,25,4,2,50);
         FullCombat.fight();
 
     }
